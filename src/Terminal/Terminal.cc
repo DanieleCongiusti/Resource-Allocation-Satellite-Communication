eff5bd4bfa517dec4aaccbd1e100603192c850e5
CharlesGiorgio <giorgiosorrentini03@gmail.com>
2025-12-04 16:27:02 +0100

    Implemented Terminal (pt.2)

--- src/Terminal/Terminal.cc
+++ src/Terminal/Terminal.cc
@@ -1,18 +1,3 @@
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see http://www.gnu.org/licenses/.
-// 
-
 // declaration of cpacket with: B, gate_index, boolean grant
 // declaration of cpacket with S bytes
 
@@ -29,12 +14,131 @@
 
 Define_Module(Terminal);
 
+void Terminal::generateGrant(ComMessage* msg) {
+    // Check if Queue is Empty
+    if (!msg_queue->last) {
+        msg->setB(-1);
+        return;  
+    } 
+
+    // IF NOT EMPTY: Pick a number among 2, 4, 8, 16 to set B with 
+    B = 2^par("B"); 
+    msg->setB(B); 
+}
+
+Terminal::~Terminal()
+{
+    // Cancel and delete self-messages
+    if (t_msg_to_q)        cancelAndDelete(t_msg_to_q);
+    if (t_time_frame)      cancelAndDelete(t_time_frame);
+    if (t_tx)              cancelAndDelete(t_tx);
+    
+    // Delete all messages inside the queue
+    if (msg_queue) {
+        ContentMessage* m;
+        while ((m = msg_queue->extractMessage()) != nullptr)
+        delete m;
+        delete msg_queue;
+    }
+
+    // Delete grant message
+    if (msg_grant)         cancelAndDelete(msg_grant); 
+}
+
 void Terminal::initialize()
 {
-    // TODO - Generated method body
+    //// FIELDS 
+    // Timers 
+    t_msg_to_q = new cMessage("contentMessage_timer");
+    t_time_frame = new cMessage("timeframe_timer"); 
+    t_tx = new cMessage("transmission");
+    
+    // Message Queue
+    msg_queue = new MessageList(); 
+    
+    // Signals 
+    // ... 
+    
+    //// LOOP BEGINNING
+    // "Create Message to Queue"
+    scheduleAt(simTime()+par("T"), t_msg_to_q);
+    
+    // "Protocol Loop (at every Time Frame)"
+    scheduleAt(simTime() + par("timeFrame_duration"), t_time_frame); 
+    msg_grant = new ComMessage("grant_request"); 
+    generateGrant(msg_grant);   // B is defined
+    send(msg_grant, "out");
 }
 
 void Terminal::handleMessage(cMessage *msg)
 {
-    // TODO - Generated method body
+    // [Create Packets]
+    if(msg == t_msg_to_q) {
+        ContentMessage* new_msg = new ContentMessage();
+        new_msg->setSize(par("S"));
+        msg_queue->addMessage(new_msg);
+        EV_INFO << "Added New Message of Size: " << new_msg->getSize() << "\n";
+
+        scheduleAt(simTime()+par("T"), t_msg_to_q); 
+    }
+
+    // [Start of a New Time Frame (T)] 
+    else if (msg == t_time_frame) {
+        // Delete any previous Transmission Timer coming from the previous Time Frame
+        if (t_tx)   cancelAndDelete(t_tx);  
+        
+        // Restart Timer for Time Frame as soon as possible
+        scheduleAt(simTime() + par("timeFrame_duration"), t_time_frame);    
+
+        // 1. Create ComMessage -> Save and Send B to GS (NB: call message "grant_request") 
+        generateGrant(msg_grant);   // B value saved
+        send(msg_grant, "out");     // B value sent
+        EV_INFO << "Terminal " << this->getName() << " has sent Grant Request to GS w/ value B = " << msg_grant->getB() << "\n"; 
+    }
+
+    // [Receive 'Grant' (GS)]  
+    else if (msg->isName("grant_request")) {
+        // 2.a IF Grant is NOT given => DO NOTHING (return)
+        if (!msg->getGrant()) return; 
+
+        // 2.b OTHERWISE => Compute M and Start Transmission Timer (t_tx), 
+        M = floor(100 * pow(par("K"), logbase(B, 2) - 1));      // M = 100*K^(log_2(B)-1)
+
+        scheduleAt(simTime(), t_tx);    // Begin Transmission Timer 
+    }
+
+    // [Receive "Transmission Notification" (T)]
+    else if (msg == t_tx) {
+        // 3. Transmit as much as 100*K^(log_2(B)-1) AND until the Queue is NOT empty 
+        //      (NB: call message "bytes")
+    
+            // NB: DON'T USE WHILE LOOP, instead create a new section (this one) in handleMessage to handle 
+            //      the transmission of packets so that it can be interruptable; the timer is "fake" as in 
+            //      the "scheduleAt()" takes as the delay time directly simTime() meaning that the module 
+            //      receives the message immediately 
+
+            ContentMessage* byte_msg = new ContentMessage("bytes"); 
+            
+            // If there are still messages to send, extract them
+            // OTHERWISE stop the routine (return)
+            if (!msg_queue->last) 
+                byte_msg = msg_queue->extractMessage(); 
+            else 
+                return; 
+
+            // If max amount M hasn't reached, send them 
+            // OTHERWISE stop the routine (return)
+            int size = byte_msg->getSize()
+            if (M > size) {
+                M -= size;
+                send(byte_msg, "out");  
+            }
+            else 
+                return; 
+
+            // Resend Transmission Notification to send next packet (OR to stop routine) 
+            scheduleAt(simTime(), t_tx); 
+        }     
+
+
 }
