aee39bb47803a74a35524b506917f99ae6c21dcd
CharlesGiorgio <giorgiosorrentini03@gmail.com>
2025-12-04 16:22:06 +0100

    Implemented Terminal module

--- src/Terminal/Terminal.cc
+++ src/Terminal/Terminal.cc
@@ -13,6 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // 
 
+// declaration of cpacket with: B, gate_index, boolean grant
+// declaration of cpacket with S bytes
 
 // T e S parameters NED file
 // Every T (expon. distributed), using scheduleAt(), message to itself (timer) to generate a packet of size S (uniform. distributed)
@@ -24,14 +26,12 @@
 // read S of the packet (FIFO policy), while(M >= S && list is not empty), extract, M -= S and send to Satellite, otherwise finish
 
 #include "../Terminal/Terminal.h"
-#include "../Message/comMessage_m.h"
-#include "../Message/contentMessage_m.h"
 
 Define_Module(Terminal);
 
 void Terminal::initialize()
 {
-
+    // TODO - Generated method body
 }
 
 void Terminal::handleMessage(cMessage *msg)
